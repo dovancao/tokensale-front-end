a946661e59fbc8c60c722cfd6ce723c68264c8b9
sonphuc <dpson1995@gmail.com>
2018-08-23 16:23:06 +0700

    commit new api

--- src/components/TokenSale.js
+++ src/components/TokenSale.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+import axios from '../axios';
 
 class TokenSale extends Component {
-    state = {
-        metamask: '',
-        web3js: null
-    }
+  state = {
+      metamask: '',
+      web3js: null
+  }
 
-    componentDidMount() {
-        if (typeof window.web3 !== 'undefined' && !this.state.web3Defined) {
+  componentDidMount() {
+    if (typeof window.web3 !== 'undefined' && !this.state.web3Defined) {
             let web3js = new Web3(window.web3.currentProvider);
             this.setState({
                 web3js: web3js,
@@ -41,45 +42,70 @@ class TokenSale extends Component {
             console.log('no network detect')
             this.setState({
               visible: true
-            })
-          }
+          })
         }
-      
+    }
+  
+    buyToken = () => {
+      axios
+        .post('https://tokensale-api.herokuapp.com/api/wallet/crowd-sale', {
 
+        })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+    }
+      
   render() {
     return (
         <div className="container">
         <div className="row js350">
             <div className="js450">
-                <h1 className="js550">Participate in a token sale</h1>
+                <h1 className="js550">Fill out the details to participate</h1>
             </div>
             <div className="js451 tokensale">
-                <div className="col-md-8">
-                    <form>
-                        <div class="form-group">
-                            <label>Select ICO token to purchase</label>
-                            <div class="btn-group">
-                            <a role="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
-                            ICOs
-                            </a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">SON</a>
-                                <a class="dropdown-item" href="#">ETH</a>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>What are you purchasing with?</label>
-                        </div>
-                        <div class="form-group">
-                            <label>Amount of NEO</label>
-                        </div>
-                    </form>
-                {/* <div className='col-md-8' id="showUp">
-                    {mHTML}
-                </div> */}
+                <div className="row">
+                  <div className="form-group col-4">
+                    <div>
+                      <label style={{fontSize: "15px", fontWeight: "bold"}}
+                      >Select ICO Token To Purchase </label>
+                    </div>
+                    <div class="dropdown">
+                      <button type="button" 
+                              className="btn btn-success dropdown-toggle"
+                              data-toggle="dropdown" 
+                              aria-haspopup="true" 
+                              aria-expanded="false"
+                      >SON Token</button>
+                      {/* <div className="dropdown-menu" ria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item">SON</a>
+                        <a className="dropdown-item">ETH</a>
+                      </div> */}
+                    </div>
+                  </div>
+                    <div className="form-group col-4">
+                      <div>
+                        <label style={{fontSize: "15px", fontWeight: "bold"}}>
+                        What are you purchasing with?</label>
+                      </div>
+                      <div>
+                        <input placeholder="ETH" disabled="disabled"></input>
+                      </div>
+                    </div>
+                    <div className="form-group col-4">
+                      <div>
+                        <label style={{fontSize: "15px", fontWeight: "bold"}}>Amount of ETH?</label>
+                      </div>
+                      <div>
+                        <input placeholder="1 ETH"></input>
+                      </div>
+                    </div>
                 </div>
             </div>
+            <button
+              onClick={this.buyToken()} 
+              type="button" 
+              class="btn btn-success">
+            I Agree And Buy Son Token Now!</button>
         </div>
     </div>
     );
--- src/components/WalletContainer.js
+++ src/components/WalletContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Link from 'react-router-dom/Link';
+import Web3 from 'web3';
 // import jwt_decode from "jwt-decode";
 
 
@@ -7,9 +8,104 @@ class WalletContainer extends Component {
     state = {
         token: '',
         address: '',
+        mHTML: 3,
+        network: '',
+        web3js: ''
     }
 
+    detectNetwork () {
+        if (typeof window.web3 !== 'undefined' && !this.state.web3Defined) {
+            this.setState({
+                mHTML: 1,
+                web3js: new Web3(window.web3.currentProvider)
+            })
+      
+            window.web3.version.getNetwork((err, netId) => {
+              switch (netId) {
+                  case "1":
+                      this.setState({network: 'This is mainnet'})
+                      break
+                  case "2":
+                      this.setState({network: 'This is the deprecated Morden test network.'})
+                      break
+                  case "3":
+                      this.setState({network: 'This is the ropsten test network.'})
+                      break
+                  case "4":
+                      this.setState({network: 'This is the Rinkeby test network.'})
+                      break
+                  case "42":
+                      this.setState({network: 'This is the Kovan test network.'})
+                      break
+                  default:
+                      this.setState({network: 'This is an unknown network.'})
+              }
+            })
+          } else {
+            // set the provider you want from Web3.providers
+            console.log('no network detect')
+            this.setState({
+              mHTML: 2
+            })
+          }
+    }
+
+    componentDidMount () {
+        this.detectNetwork();
+        setInterval(this.detectNetwork(), 2000);
+      }
+
   render() {
+    let html;
+    if(this.state.mHTML === 2){
+        html = (
+            <div style={{textAlign: 'center'}}>
+              <div className="row js350">
+                <div className="col-6 js451">
+                  <div style={{borderBottom: '2px'}}>
+                    <h2>OPPS</h2>
+                    <p>You'll Need A Place To Store All Of Your Token</p>
+                  </div>
+                  <div>
+                    <img src="https://thumbs.gfycat.com/FlippantInformalAsianwaterbuffalo-size_restricted.gif" alt="animated"/>
+                  </div>
+                  <div>
+                    The perfect place is in a secure wallet like MetaMask. 
+                    This will also act as your login to the game (no extra password needed).
+                  </div>
+                  <div>
+                    <a className="btn btn-success active" href="https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/">
+                    <button>Get from Firefox Or Google Add-ons Store</button>
+                    </a>
+                  </div>
+                  <div>Or</div>
+                </div>
+                <div className="col-6 js451">
+                    <h4>Unlock your wallet to claim GAS</h4>
+                    <form>
+                        <div className="form-group js401">
+                            <label htmlFor="password">Enter Password</label>
+                            <input type="password" className="form-control" id="password" />
+                        </div>
+                        <button type="submit" className="btn btn-success">Unlock</button>
+                    </form>
+                  </div>
+              </div>
+            </div>
+        )
+    } else if(this.state.mHTML === 1){
+      html = (
+        <div className="row js350">
+        <div className="col-6 js451">
+          <div style={{borderBottom: '2px'}}>PERFECT</div> 
+          <div>
+            <h4>Your wallet is unlocked! Please partitipate in token sale</h4>
+          </div> 
+        </div>
+        </div>
+      )
+    }
+
     return (
         <div className="container">
             <div className="row js350">
@@ -25,17 +121,8 @@ class WalletContainer extends Component {
                     </div>
                 </div>
             </div>
-            <div className="row js350">
-                <div className="col-6 js451">
-                    <h4>Unlock your wallet to claim GAS</h4>
-                    <form>
-                        <div className="form-group js401">
-                            <label htmlFor="password">Enter Password</label>
-                            <input type="password" className="form-control" id="password" />
-                        </div>
-                        <button type="submit" className="btn btn-success">Unlock</button>
-                    </form>
-                </div>
+            <div>
+              {html}
             </div>
         </div>
     );
